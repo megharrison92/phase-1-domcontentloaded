c73b86d30f810ce54034bbf3013183333d51ac69
Megan Harrison <mtharrison@alumni.fullsail.edu>
2023-05-26 11:34:31 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    let cupOfCoffee = document.getElementById("text")
+    cupOfCoffee.textContent = "This is really cool!"
+    console.log(cupOfCoffee)
+});
+
+
+
+
+
+
+
